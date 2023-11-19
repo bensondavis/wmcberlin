5f1c64f1b6a7ed9187970e0a5f8b3d45a2814be1
Benson Davis <Benson.Davis@soti.net>
2023-11-20 02:36:06 +0530

    compressed imgs

--- src/data/gallery/gallery-data.tsx
+++ src/data/gallery/gallery-data.tsx
@@ -2,9 +2,10 @@ import pic1 from "../../assets/gallery/DSC06484.jpg";
 import pic2 from "../../assets/gallery/DSC06499.jpg";
 import pic3 from "../../assets/gallery/DSC06519.jpg";
 import pic4 from "../../assets/gallery/DJI_0725.jpg";
-import pic5 from "../../assets/gallery/DSC02737.jpeg";
+import pic5 from "../../assets/gallery/DSC02737.jpg";
 import pic6 from "../../assets/gallery/DSC02794.jpg";
+import pic7 from "../../assets/gallery/IMG_4405.jpg";
 
-const GalleryData = [pic1, pic2, pic3, pic4, pic5, pic6];
+const GalleryData = [pic1, pic2, pic3, pic4, pic5, pic6, pic7];
 
 export default GalleryData;
