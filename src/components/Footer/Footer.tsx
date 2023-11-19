b0e0fb48117114d2bce5a94d6ce125af49057d4e
Benson Davis <Benson.Davis@soti.net>
2023-11-20 02:22:20 +0530

    fixed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,19 @@
 import { Box, Stack, Typography } from "@mui/material";
-import styles from "./Footer.module.css"
-import EmailIcon from '@mui/icons-material/Email';
+import styles from "./Footer.module.css";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Footer = () => {
   return (
     <Box className={styles["footer-container"]}>
-      <Stack direction={"row"}>
+      <Stack direction={{ md: "row", sm: "row", xs: "column" }} className={styles["margin-y-10"]}>
         <Typography className={styles.text}>WMC Berlin</Typography>
-        <Typography>•</Typography>
-        <EmailIcon className={styles.icon} />
-        <Typography className={styles.text}>worldmalayaleecouncil.berlin@gmail.com</Typography>
+        <Typography className={styles.dot}>•</Typography>
+        <Box className={styles.email}>
+          <EmailIcon className={styles.icon} />
+          <Typography className={styles.text}>
+            worldmalayaleecouncil.berlin@gmail.com
+          </Typography>
+        </Box>
       </Stack>
     </Box>
   );
